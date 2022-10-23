a58b7d9eb3ec55c619d6f391359b44767b03660b
Shawn Corrigan <shawn.corrigan@wmg.com>
2022-10-23 15:24:53 -0400

    Upgrade to Tabulator version 5.4.2 and Manifest V3, * Replace Tabulator version with 5.4.2 * Change manifest.json to match Manifest V3. * Replace background script with service worker, rename background.js to worker.js * Rename content.css to common.css and move to common folder (this file was related to the functionality in common.js, and not content.js) * Use chrome.scripting where necessary for Manifest V3 * Load scripts only once per tab, load directly on tab (this follows Manifest V3 security measures). * Use promises to ensure that the scripts are loaded before the Tabulator table is initialized.

--- src/common/common.js
+++ src/common/common.js
@@ -21,12 +21,13 @@ function initTable(el) {
                 }
             }); 
 
-            chrome.storage.sync.get({
-                forceWidth: false,
-                sorting: true,
-                filters: true,
-                paging: true
-            },
+            chrome.storage.sync.get(
+                {
+                    forceWidth: false,
+                    sorting: true,
+                    filters: true,
+                    paging: true
+                },
                 function (items) {
                     if (items.forceWidth) {
                         // set the table to 100% width
@@ -37,63 +38,27 @@ function initTable(el) {
                     var options = {
                         layout: "fitColumns",
                         movableColumns: false,
-                        resizableColumns: true,
-                        headerFilterPlaceholder: "",
+                        columnDefaults:{
+                            resizable: "header",
+                            formatter: "html",
+                            width: null,
+                            tooltip: true,
+                            headerFilterPlaceholder: "",
+                            headerFilter: "input",
+                        },            
                         clipboard: true, //enable clipboard functionality
-                        clipboardCopySelector: "table", //change default selector to active
+                        clipboardCopyRowRange: "all", //change default selector to active
                         clipboardCopyStyled: false,
-                        clipboardCopyFormatter: function (data) {
-                            var output = [];
-
-                            // temporary patch until Tabulator fixes this bug
-                            // https://github.com/olifolkerd/tabulator/pull/2399
-                            data.forEach(function (row) {
-                                var newRow = [];
-                                row.forEach(function (value) {
-                                    if (typeof value == "undefined") {
-                                        value = "";
-                                    }
-
-                                    value = typeof value == "undefined" || value === null ? "" : value.toString();
-
-                                    if (value.match(/\r|\n/)) {
-                                        value = value.split('"').join('""');
-                                        value = '"' + value + '"';
-                                    }
-                                    newRow.push(value);
-                                });
-                                output.push(newRow.join("\t"));
-                            });
-
-                            return output.join("\n");
-
-                        },
-                        clipboardCopied: function (clipboard) {
-                            // clipboard - the string that has been copied into the clipboard
-                        },
                     };
 
                     if (items.paging) {
                         // enable paging
-                        options.pagination = "local";
-                        options.paginationSize = 100;
+                        options.pagination = true,
+                        options.paginationMode = "local",
+                        options.paginationSize = 10;
                     }
 
                     table = new Tabulator($t.get(0), options);
-                    
-                    var columns = table.getColumnDefinitions();
-                    columns.forEach(column => {
-                        column.width = null;
-                        column.tooltip = true;
-                        column.formatter = "html";
-                        if (items.filters) {
-                            column.headerFilter = "input";
-                        }
-                    });
-                    table.setColumns(columns);
-
-                    // force a redraw to fix row vertical spacing issues
-                    table.redraw();
                 });
 
         }
